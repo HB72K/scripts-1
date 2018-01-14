b5cf43a1c3d3f942333bfed77d2e5cd590d36d2f
Carlos Arriaga <carlosarriagacm@gmail.com>
2018-01-13 18:27:15 -0600

    Update LineageOS-15.1.sh

--- LineageOS-15.1.sh
+++ LineageOS-15.1.sh
@@ -3,14 +3,6 @@
 # Patches required for build Lineage-15.1
 #
 
-echo "Patches for system_qcom...";
-cd system/qcom &&
-git fetch https://github.com/LineageOS/android_system_qcom refs/changes/34/187634/1 && git cherry-pick FETCH_HEAD &&
-git fetch https://github.com/LineageOS/android_system_qcom refs/changes/35/187635/1 && git cherry-pick FETCH_HEAD &&
-git fetch https://github.com/LineageOS/android_system_qcom refs/changes/36/187636/1 && git cherry-pick FETCH_HEAD &&
-git fetch https://github.com/LineageOS/android_system_qcom refs/changes/37/187637/1 && git cherry-pick FETCH_HEAD &&
-cd ../.. &&
-
 echo "Patches for build_make...";
 cd build/make &&
 git fetch https://github.com/LineageOS/android_build refs/changes/13/196713/1 && git cherry-pick FETCH_HEAD &&
